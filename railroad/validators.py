1a24678724fa25e5704a011d23f8ddef3c147892
solyariy <solalex005@gmai.com>
2025-11-29 20:28:39 +0200

    new validation to check whether train is free from other journeys

--- railroad/validators.py
+++ railroad/validators.py
@@ -1,4 +1,6 @@
 from rest_framework.serializers import ValidationError
+from django.db.models import Q
+
 
 # available tickets
 # train is free at the time of journey
@@ -43,14 +45,30 @@ class JourneyValidatorMixin:
     @staticmethod
     def validate_time(attrs):
         if attrs.get("departure_time") >= attrs.get(
-            "arrival_time"
+                "arrival_time"
         ):
             raise ValidationError(
                 "You can't arrive before departure"
             )
 
+    @staticmethod
+    def validate_train_schedule(attrs):
+        train = attrs.get("train")
+        if not train.journeys.exists():
+            return
+        conditions = [
+            Q(arrival_time__gte=attrs.get("departure_time")),
+            Q(departure_time__lte=attrs.get("arrival_time")),
+        ]
+        if train.journeys.filter(conditions[0] & conditions[1]).exists():
+            raise ValidationError(
+                "Train will be preoccupied on these dates"
+            )
+
+
     def validate(self, attrs):
         self.validate_time(attrs)
+        self.validate_train_schedule(attrs)
         return attrs
 
 
@@ -58,8 +76,8 @@ class RouteValidatorMixin:
     @staticmethod
     def validate_stations(attrs):
         if (
-            attrs.get("source").id
-            == attrs.get("destination").id
+                attrs.get("source").id
+                == attrs.get("destination").id
         ):
             raise ValidationError(
                 "Source can't be equal to destination"
